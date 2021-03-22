749733f6cdb3d42bba0f09576dd4e7439c05dda8
Purnima2k21 <purnimas74@yahoo.com>
2021-03-22 10:50:34 -0700

    adoptions applications schema

---
+++ server/db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS adoption_applications;
+
+CREATE TABLE adoption_applications(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (255) NOT NULL,
+  phone_number INTEGER NOT NULL,
+  email VARCHAR (255) NOT NULL,
+  home_status VARCHAR (255) NOT NULL
+  application_status VARCHAR (255) NOT NULL,
+  pet_id INTEGER REFERENCES pet_types(id)
+);
