29dc75cd34097e09b681187141327d9e5c949f00
justynekyle <justynekyle@gmail.com>
2021-03-22 10:34:22 -0700

    pet_surrender_applications

---
+++ server/db/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS pet_surrender_applications;
+
+CREATE TABLE pet_surrender_applications (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone_number INTEGER NOT NULL,
+  email VARCHAR(255) NOT NULL, 
+  pet_name VARCHAR(255) NOT NULL,
+  pet_age VARCHAR(255),
+  pet_type_id INTEGER,
+  pet_image_url VARCHAR(255),
+  vaccination_status BOOLEAN,
+  application_status VARCHAR(255),
+);
