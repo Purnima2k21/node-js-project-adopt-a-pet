bba9d50439499dabb1384756113a0755594e36b2
Purnima2k21 <purnimas74@yahoo.com>
2021-03-22 11:15:36 -0700

    completed schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,3 @@
-
-
 DROP TABLE IF EXISTS pet_types;                     
 CREATE TABLE pet_types (
   id SERIAL PRIMARY KEY,
@@ -7,15 +5,37 @@ CREATE TABLE pet_types (
   description text
 );
 
-DROP TABLE IF EXISTS pet_surrender_applications;
+DROP TABLE IF EXISTS adoptable_pets;
+CREATE TABLE adoptable_pets(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (255) NOT NULL,
+  img_url VARCHAR (255) NOT NULL,
+  age INTEGER,
+  vaccination_status BOOLEAN,
+  adoption_story TEXT NOT NULL,
+  adoption_status VARCHAR NOT NULL,
+  type_id INTEGER REFERENCES pet_types(id)
+);
 
+DROP TABLE IF EXISTS adoption_applications;
+CREATE TABLE adoption_applications(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (255) NOT NULL,
+  phone_number VARCHAR (15) NOT NULL,
+  email VARCHAR (255) NOT NULL,
+  home_status VARCHAR (255) NOT NULL
+  application_status VARCHAR (255) NOT NULL,
+  pet_id INTEGER REFERENCES adoptable_pets(id)
+);
+
+DROP TABLE IF EXISTS pet_surrender_applications;
 CREATE TABLE pet_surrender_applications (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  phone_number INTEGER NOT NULL,
+  phone_number VARCHAR (15) NOT NULL,
   email VARCHAR(255) NOT NULL, 
   pet_name VARCHAR(255) NOT NULL,
-  pet_age VARCHAR(255),
+  pet_age INTEGER,
   pet_type_id INTEGER REFERENCES pet_types(id),
   pet_image_url VARCHAR(255),
   vaccination_status BOOLEAN,
@@ -23,15 +43,4 @@ CREATE TABLE pet_surrender_applications (
 );
 
 
-DROP TABLE IF EXISTS adoption_applications;
-
-CREATE TABLE adoption_applications(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR (255) NOT NULL,
-  phone_number INTEGER NOT NULL,
-  email VARCHAR (255) NOT NULL,
-  home_status VARCHAR (255) NOT NULL
-  application_status VARCHAR (255) NOT NULL,
-  pet_id INTEGER REFERENCES adoptable_pets(id)
-);
 
