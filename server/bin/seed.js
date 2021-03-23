9914af30c2e42cae90ac8d838fd39d9358f8d1c3
justynekyle <justynekyle@gmail.com>
2021-03-23 14:43:53 -0700

    Data has been seeded, Index, Model, and Tile in Progress

--- server/bin/seed.js
+++ server/bin/seed.js
@@ -1,9 +1,9 @@
 import Seeder from "../db/Seeder.js"
 
-Seeder.seed1()
-Seeder.seed2()
-Seeder.seed3()
-Seeder.seed4()
+// Seeder.seed1()
+// Seeder.seed2()
+// Seeder.seed3()
+// Seeder.seed4()
 
 // new Promise(() => {
 //   Seeder.seed1()
